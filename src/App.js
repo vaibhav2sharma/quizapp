701085f2369be16ecd6fe90f70e9590241279d11
vaibhav2sharma <108828319+vaibhav2sharma@users.noreply.github.com>
2025-01-13 15:07:03 +0530

    project made

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import "./App.css";
+import Leaderboard from "./Leaderboard";
+import Quiz from "./Quiz";
+import SetupQuiz from "./SetupQuiz";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" exact Component={SetupQuiz} />
+          <Route path="/quiz" exact Component={Quiz} />
+          <Route path="/leaderboard" exact Component={Leaderboard} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/SetupQuiz.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SetupQuiz = () => {
+  return (
+    <div>
+      setupq
+    </div>
+  )
+}
+
+export default SetupQuiz
